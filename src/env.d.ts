02fd94d77963f5432844f639a62cf39297a578fe
Krystian Piątek <piatekkrystian13@gmail.com>
2025-09-13 22:25:27 +0200

    Add env file

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  readonly AUTHOR_URL: string;
+  readonly OG_IMAGE_URL: string;
+  readonly RADIO_PLAYER_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
