5281a1ec943e5bf64ceb58a2e32ed4a11fba8dae
Krystian Piątek <piatekkrystian13@gmail.com>
2025-09-15 21:12:45 +0200

    Separate Minecraft section into reusable components

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
   readonly AUTHOR_URL: string;
   readonly OG_IMAGE_URL: string;
   readonly RADIO_PLAYER_URL: string;
+  readonly MC_FILES_CDN: string;
 }
 
 interface ImportMeta {
